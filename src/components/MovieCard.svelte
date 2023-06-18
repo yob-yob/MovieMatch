dab5647b851f0a9efa663eac48184f5ddcb39a7d
Sean <mr.seangrover@icloud.com>
2023-06-18 17:08:19 +0800

    Making it work

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -3,28 +3,27 @@
   export let movie: movie;
   
   import { configuration } from '../sdk/tmdb';
-
   
   function getMoviePoster(poster_path: string, poster_size: string) {
     let base_url = configuration.images.base_url;
     return `${base_url}${poster_size}${poster_path}`;
   }
 
-  let title = movie.media_type === 'movie' 
+  $: title = movie.media_type === 'movie' 
     ? movie.original_title 
     : movie.original_name
 </script>
 
 <div {...$$restProps}>
-  <div class="card image-full w-52 md:w-72 bg-base-100 shadow-xl">
+  <div class="card w-52 md:w-96 bg-base-100 shadow-xl">
     <figure>
       <img 
         src={getMoviePoster(movie.poster_path, 'original')} 
         alt="{title}" 
-        class="h-20 object-contain"
+        class="object-contain"
       />
     </figure>
-    <div class="card-body">
+    <div class="card-body bg-gray-800">
       <h2 class="card-title">{title}</h2>
       <p>{movie.overview.substring(0,200)}...</p>
       <div class="card-actions justify-end">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,42 +11,60 @@
     return Math.floor(Math.random() * 11) - 5;
   }
 
+  let movies = data.movies;
   let area;
   let draggingInfo: PanInfo | null = null;
   let DragStatus = 'middle';
-  $: if (draggingInfo) {
-    DragStatus = draggingInfo.offset.x > 0 ? 'right' : 'left'
-  } else {
-    DragStatus = 'middle'
+  let dragOffset = 300;
+  let rightItems = [];
+  let leftItems = [];
+  $: dragOffsetX = draggingInfo ? draggingInfo.offset.x : 0;
+  $: DragStatus = (dragOffsetX >= dragOffset ? 'right' : (dragOffsetX <= -dragOffset ? 'left' : 'middle'))
+  $: currentMovie = movies.length > 0 ? movies[0] : null;
+
+  function dragEnded(event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) {
+    if (DragStatus === 'right') {
+      rightItems.push(currentMovie)
+    } else if (DragStatus === 'left') {
+      leftItems.push(currentMovie)
+    }
+    console.log('Movie List updated', currentMovie?.original_title, rightItems.length, leftItems.length)
+    movies.shift();
+    movies = movies;
+    console.log('Movie List updated', movies[0].original_title, rightItems.length, leftItems.length)
+    draggingInfo = null
   }
 </script>
 
-
 <!-- Page to swipe Cards -->
 <div class="grid place-content-center h-full w-full">
-    <div class="relative">
-      <div bind:this={area} class="absolute -translate-y-1/2 -translate-x-1/2">
-        <Motion 
-          let:motion 
-          drag 
-          dragConstraints={{current:area}}
-          dragElastic={1}
-          onDragStart={(event, info) => draggingInfo = info}
-          onDrag={(event, info) => draggingInfo = info}
-          onDragEnd={(event, info) => draggingInfo = null}
-        >
-          <div 
-            use:motion 
-            style:transform={`rotate(${generateRandomRotationValue()}deg)`}>
+    {#if currentMovie}
+      <div class="relative">
+        <div bind:this={area} class="absolute -translate-y-1/2 -translate-x-1/2">
+          <Motion 
+            let:motion 
+            drag 
+            dragConstraints={{current:area}}
+            dragElastic={1}
+            onDragStart={(event, info) => draggingInfo = info}
+            onDrag={(event, info) => draggingInfo = info}
+            onDragEnd={(event, info) => dragEnded(event, info)}
+          >
             <div 
-              class="absolute bg-gradient-to-b via-transparent z-40 w-full h-full rounded-2xl"
-              class:from-green-400="{DragStatus === 'right'}"
-              class:from-red-400="{DragStatus === 'left'}"
-            >
+              use:motion 
+              style:transform={`rotate(${generateRandomRotationValue()}deg)`}>
+              <div 
+                class="absolute bg-gradient-to-b via-transparent z-40 w-full h-full rounded-2xl transition-all duration-500"
+                class:from-green-300="{DragStatus === 'right'}"
+                class:from-red-300="{DragStatus === 'left'}"
+              >
+              </div>
+              <MovieCard movie={currentMovie} />
             </div>
-            <MovieCard movie={data.movies[0]} />
-          </div>
-        </Motion>
+          </Motion>
+        </div>
       </div>
-    </div>
+    {:else}
+      No movies
+    {/if}
 </div>
